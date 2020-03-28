1eb8f6ebf0168ed6ed86be1396504e22068ba253
John Hughes <john.hughes101@gmail.com>
2020-03-28 13:50:04 +0000

    added gorilla mux router for dynamic routes

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,42 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
 
+	"github.com/gorilla/mux"
+
+	"github.com/Jonnay101/request-handler-with-error-rail.git/pkg/database"
+	"github.com/Jonnay101/request-handler-with-error-rail.git/pkg/handlers"
+)
+
+func main() {
+	// get env vars
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	// create required utilities
+	imageRepository := database.CreateNewImageRepository("tmp/images")
+	handlers := handlers.NewHandlers(imageRepository)
+	// set routes
+	muxRouter := mux.NewRouter()
+	muxRouter.HandleFunc("/{person}", hello)
+	muxRouter.HandleFunc("/images/user/{user_uuid}/image/{image_uuid}", handlers.UploadImage)
+	// create and start server
+	srv := &http.Server{
+		Handler:      muxRouter,
+		Addr:         fmt.Sprintf("127.0.0.1:%s", port),
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+	log.Fatal(srv.ListenAndServe())
+}
+
+// hello test handler
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "hello, this is the thingy!!!")
 }
--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.13
 
 require (
 	github.com/globalsign/mgo v0.0.0-20181015135952-eeefdecb41b8 // indirect
+	github.com/gorilla/mux v1.7.4
 	github.com/music-tribe/uuid v1.1.1
 )
