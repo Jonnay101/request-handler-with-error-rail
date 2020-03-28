e187aab606a781cd87703cd79d04ba098f9c63a6
John Hughes <john.hughes101@gmail.com>
2020-03-28 15:01:43 +0000

    setup url param binding

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,10 +1,12 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/Jonnay101/request-handler-with-error-rail.git/pkg/database"
+	"github.com/gorilla/mux"
 	"github.com/music-tribe/uuid"
 )
 
@@ -16,6 +18,7 @@ type Handlers interface {
 // Request - methods required to satisfy interface
 type Request interface {
 	BindRequestBody() Request
+	ReturnError() error
 }
 
 // handlers will satisfy the Handlers interface
@@ -30,18 +33,29 @@ func NewHandlers(db database.Database) Handlers {
 
 type request struct {
 	HTTPRequest *http.Request `json:"http_request,omitempty"`
-	UserUUID    uuid.UUID     `json:"user_uuid,omitempty"`
-	FileUUID    uuid.UUID     `json:"file_uuid,omitempty"`
+	UserUUID    uuid.UUID     `json:"user_uuid,omitempty" bson:"user_uuid"`
+	FileUUID    uuid.UUID     `json:"file_uuid,omitempty" bson:"file_uuid"`
 	Filename    string        `json:"filename,omitempty"`
 	FileData    []byte        `json:"file_data,omitempty"`
+	Err         error         `json:"_, omitempty"`
 }
 
 func newRequest(r *http.Request) *request { return &request{HTTPRequest: r} }
 func (r *request) GetFileID() uuid.UUID   { return r.FileUUID }
 func (r *request) GetFilename() string    { return r.Filename }
 func (r *request) GetFileData() []byte    { return r.FileData }
+func (r *request) ReturnError() error     { return r.Err }
 func (r *request) BindURLParams() Request {
 	// bind request params
+	mapOfDynaimcParams := mux.Vars(r.HTTPRequest)
+	var byt []byte
+	// marshal map of params to json
+	byt, r.Err = json.Marshal(mapOfDynaimcParams)
+	if r.Err != nil {
+		return r
+	}
+	// unmarshal the json to our request struct
+	r.Err = json.Unmarshal(byt, &r)
 	return r
 }
 func (r *request) BindQueryParams() Request {
@@ -56,6 +70,7 @@ func (r *request) BindRequestBody() Request {
 // UploadImage takes a multipart image file and stores it in the db
 // the image will also be displayed to the user
 func (h *handlers) UploadImage(w http.ResponseWriter, r *http.Request) {
-	uploadRequest := newRequest(r)
-	fmt.Printf("%v+", uploadRequest)
+	fmt.Fprint(w, "this is hit")
+	uploadRequest := newRequest(r).BindURLParams()
+	fmt.Fprintf(w, "%v", uploadRequest)
 }
